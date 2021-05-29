4153f63c17aa7300a15796f962e76d0413563a07
Daesons <damned766@gmail.com>
2021-05-30 01:26:14 +0300

    zavtra dodelat' contacts

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -7,16 +7,40 @@ export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <Separator/>
+
             <div className={s.container}>
                 <Title title={'Contacts'}/>
-                <div className={s.formContainer}>
-                    <form className={s.form}>
-                        <input/>
-                        <input/>
-                        <textarea className={s.textarea}/>
-                    </form>
+                <div className={s.wrapper}>
+                    <div className={s.description}>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit.
+                        Aliquam architecto dolorem ducimus
+                        earum laboriosam laborum nemo optio ratione sequi voluptas? A,
+                        cupiditate dignissimos dolor enim maxime reprehenderit sint tempora voluptate.
+                    </div>
+                    <div className={s.formContainer}>
+                        <form className={s.form}>
+                            <div className={s.inputContainer}>
+                                <div className={s.input}>
+                                    <input placeholder={'YOUR NAME'}/>
+                                </div>
+                                <div className={s.input}>
+                                        <input placeholder={'YOUR EMAIL'}/>
+                                </div>
+                                <div className={s.input}>
+                                    <input placeholder={'YOUR SUBJECT'}/>
+                                </div>
+                            </div>
+                            <div  className={s.textarea}>
+                                <textarea placeholder={'YOUR MESSAGE'}/>
+                            </div>
+                            <div>
+                                <button type={'submit'}> kek</button>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
-                <p><input type="submit"/></p>
             </div>
         </div>
     )
