957ecfbb6bdb92639b4234854ba41f041fe32d69
Daesons <damned766@gmail.com>
2021-05-31 00:28:50 +0300

    continue

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,16 +5,30 @@ import {Title} from "../Common/Title/Title";
 import {Separator} from "../Common/Separator/Separator";
 import {lorem} from "../../App";
 
+import cardsProjectImg from '../../assets/CardsProject.jpg'
+import ToDoImg from '../../assets/ToDo.jpg'
+import SocialNetworkImg from '../../assets/socialNetwork.jpg'
+
+
 export const Projects = () => {
+    const cardsProject = {
+        backgroundImage : `url(${cardsProjectImg})`
+    }
+    const ToDo = {
+        backgroundImage : `url(${ToDoImg})`
+    }
+    const SocialNetwork = {
+        backgroundImage: `url(${SocialNetworkImg})`
+    }
     return (
         <div className={styles.projectsBlock}>
            <Separator/>
             <div className={styles.container}>
                <Title title={'Projects'}/>
                 <div className={styles.projects}>
-                    <Project description={lorem} title={'Social-network'}/>
-                    <Project description={lorem} title={'ne Social-network'}/>
-                    <Project description={lorem} title={'ne Social-network'}/>
+                    <Project style={SocialNetwork} description={lorem} title={'Social-network'}/>
+                    <Project style={cardsProject} description={lorem} title={'ne Social-network'}/>
+                    <Project style={ToDo} description={lorem} title={'ne Social-network'}/>
                 </div>
             </div>
         </div>
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -4,14 +4,18 @@ import styles from './Project.module.scss'
 type ProjectPropsType = {
     title: string
     description: string
+    style: {
+        backgroundImage : string
+    }
+
 }
 
 export const Project = (props: ProjectPropsType) => {
     return (
         <div className={styles.projectBlock}>
             <div className={styles.container}>
-                <div className={styles.picture}>
-                    <button>See more</button>
+                <div className={styles.picture} style={props.style}>
+                    <a className={styles.button}>See more</a>
                 </div>
                 <div className={styles.projectDescription}>
                     <h3 className={styles.title}>{props.title}</h3>
